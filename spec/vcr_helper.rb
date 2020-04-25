e223879d42c69fd0c27a2be3411de44cf3e6cb2d
Dominik Reller <Dom-R@users.noreply.github.com>
2020-04-25 20:48:45 -0300

    configure vcr to record new episodes instead of once
    I had to change the default configuration that VCR uses to record due to
    a possible conflict between VCR and Capybara. I have searched a little
    and it seems that others had the same problem before.
    
    Or it could be a simple intermittent problem with contentful_model that
    I didn't have before.

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -4,6 +4,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.default_cassette_options = { record: :new_episodes }
 
   config.filter_sensitive_data('<CONTENTFUL_ACCESS_TOKEN>') { ENV.fetch('CONTENTFUL_ACCESS_TOKEN') }
   config.filter_sensitive_data('<CONTENTFUL_SPACE') { ENV.fetch('CONTENTFUL_SPACE') }
